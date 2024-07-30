370b91f94e220d000e15ee1417deae01e8717043
unknown <105841112621@student.unismuh.ac.id>
2024-07-30 18:43:36 +0800

    Use Fonts

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
-import { View } from 'react-native'
-import React from 'react'
-import Login from './component/pages/login'
+import { useFonts } from 'expo-font';
+import { Text, View } from 'react-native';
 
-const App = () => {
- return (
-  <View style = {{
-    flex: 1,
-    backgroundColor: '#EEEEEE'
-  }}>
-    <Login />
-  </View>
- );
+export default function App () {
+  const [dapatFont] = useFonts ({
+    'MetroBlack' : require('./assets/fonts/Metropolis-Black.otf'),
+    'MetroBold' : require('./assets/fonts/Metropolis-Bold.otf'),
+    'MetroLight' : require('./assets/fonts/Metropolis-Light.otf'),
+    'MetroMedium' : require('./assets/fonts/Metropolis-Medium.otf'),
+    'MetroSemiBold' : require('./assets/fonts/Metropolis-SemiBold.otf'),
+  });
+
+  if (!dapatFont) 
+    return <Text>font Tidak Ditemukan Silahkan Coba Lagi...</Text>
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ fontFamily: 'MetroBold' }}>Font Metropolis Black</Text>
+          <Text style={{ fontFamily: 'MetroBold' }}>Font Metropolis Bold</Text>
+          <Text style={{ fontFamily: 'MetroLight' }}>Font Metropolis Light</Text>
+          <Text style={{ fontFamily: 'MetroMedium' }}>Font Metropolis Medium</Text>
+          <Text style={{ fontFamily: 'MetroSemiBold' }}>Font Metropolis SemiBold</Text>
+      <Text> Font Biasa </Text>
+      </View>
+    )
 }
-
-export default App
